22fbff1434dac8e6a8908a2d27b015b078aa4e48
Joseph Marinier <Joseph.Marinier@gmail.com>
2022-12-04 00:30:34 -0500

    2022-12-04

---
+++ 2022/4part2.ts
@@ -0,0 +1,14 @@
+fetch(`${window.location.pathname}/input`).then((r) => r.text()).then((text) => {
+    const input = text.trim().split("\n").map((line) => {
+        const [[a, b], [c, d]] = line.split(",").map((range) => range.split("-").map(Number));
+        return !(b < c || d < a);
+    });
+
+    const answer = input.reduce((a, b) => a + b);
+
+    console.log(answer);
+    const field = document.querySelector(`input[name="answer"]`);
+    if (field) field.value = String(answer);
+    const submit = document.querySelector(`input[type="submit"]`);
+    if (submit) submit.click();
+});
