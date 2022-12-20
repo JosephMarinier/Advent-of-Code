8b39d2010b314fdd5400ec77f8b7757caa143261
Joseph Marinier <Joseph.Marinier@gmail.com>
2022-12-20 08:04:52 -0500

    2022-12-20

---
+++ 2022/20part2.ts
@@ -0,0 +1,27 @@
+fetch(`${window.location.pathname}/input`).then((r) => r.text()).then((text) => {
+    const input = text.trim().split("\n").map(Number).map((move) => move * 811589153);
+
+    const order = [...input.keys()];
+
+    for (let i = 0; i < 10; i++) {
+        input.forEach((move, i) => {
+            const before = order.indexOf(i);
+            const after = (before + move) % (input.length - 1);
+            // Optionally rearrange position 0 to match the example:
+            // if (after === 0) {
+            //     after = input.length - 1;
+            // }
+            order.splice(after, 0, ...order.splice(before, 1));
+        });
+    }
+
+    const indexOf0 = order.indexOf(input.indexOf(0));
+    const answer = [1000, 2000, 3000].map((pos) => input[order[(indexOf0 + pos) % input.length]]).reduce((a, b) => a + b);
+
+    console.log(answer); // 1240919814937
+    if (!answer) return;
+    const field = document.querySelector(`input[name="answer"]`);
+    if (field) field.value = String(answer);
+    const submit = document.querySelector(`input[type="submit"]`);
+    if (submit) submit.click();
+});
